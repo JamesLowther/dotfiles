ae93d53f9325ad5911f7991732e7035b292fda7f
James Lowther <jamesevlowther@gmail.com>
2023-06-19 13:42:25 -0600

    Add nvim

--- mklinks.sh
+++ mklinks.sh
@@ -8,8 +8,15 @@ for dir in "${dirs[@]}"; do
   src="$HOME/dotfiles/$dir" 
   dst="$HOME/$dir"
 
-  if [ -d "$dst" ]; then
-    echo "$dst already exists. Please remove it before linking"
+  if [ -d "$dst" ] || [ -f "$dst" ]; then
+    read -e -p "$dst already exists. Would you like to delete it? [y/N]:" choice
+    if [[ "$choice" == [Yy]* ]]; then
+      echo "Deleting $dst"
+      rm -r $dst
+      ln -sv $src $dst
+    else
+      echo "Skipping delete"
+    fi
   else
     ln -sv $src $dst
   fi
