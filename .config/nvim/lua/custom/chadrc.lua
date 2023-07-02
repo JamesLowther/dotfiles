e1c1d16ee2f06109f6d6e28619cb53d25ee09b9b
James Lowther <jamesevlowther@gmail.com>
2023-07-01 18:30:47 -0600

    docker

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -1,5 +1,10 @@
 ---@type ChadrcConfig 
  local M = {}
- M.ui = {theme = 'nightfox'}
+ M.ui = {
+  theme = 'nightfox',
+  statusline = {
+    theme = "minimal",
+  }
+}
  M.plugins = "custom.plugins"
  return M
